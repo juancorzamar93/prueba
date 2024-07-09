fcb1efd455837a37aac0e1916580b8c75fe91886
Juan Cortez Zamar <106788005+juancorzamar93@users.noreply.github.com>
2024-07-09 02:40:27 +0000

    .

--- desafio_preEntrega3/dags/modules/extract_data.py
+++ desafio_preEntrega3/dags/modules/extract_data.py
@@ -1,8 +1,7 @@
-
 from modules.data_from_api import exchange_rate_data
 from parameters import API_URL
 
-def extract_data(api_url):
+def extract_data(API_URL):
     data = exchange_rate_data(API_URL)
     return data
 
